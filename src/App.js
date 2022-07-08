d950fed00d9fd479820c726ea540e4e45192936c
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2022-07-08 11:40:16 +0800

    filter by year component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import './components/Expenses/Expenses.css';
-import ExpenseItem from './components/Expenses/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 import React from 'react';
@@ -10,11 +9,20 @@ import React from 'react';
 const App = () => {
 
   const expenses = [
-    {title:'Car Insurance', amount:94.12, date: new Date(2022, 5, 12)},
-    {title:'Toy', amount:14.12, date: new Date(2022, 5, 13)},
-    {title:'Subscribe', amount:114.12, date: new Date(2022, 5, 14)},
-    {title:'Bag', amount:24.12, date: new Date(2022, 5, 15)}
-  ]
+    {title:'Car Insurance', amount:94.12, date: new Date(2019, 2, 12)},
+    {title:'Toy', amount:14.12, date: new Date(2020, 3, 13)},
+    {title:'Toy2', amount:15.12, date: new Date(2020, 5, 13)},
+    {title:'Subscribe', amount:114.12, date: new Date(2021, 8, 14)},
+    {title:'Bag', amount:24.12, date: new Date(2022, 11, 15)}
+  ];
+
+  const filteredData = expenses.filter(expense => expense.date.getFullYear() == '2022');
+  console.log(filteredData);
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  }
 
   // return React.createElement(
   //   'div', {}, 
@@ -24,14 +32,7 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* {expenses.map((expense, i) => {
-        return (
-          <div key={i} className='expenses'>
-            <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
-          </div>
-        )
-      })} */}
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
